4ab2f67a782a1231d76acca7ce5f047e5d2a41a9
jas8157 <jas8157@naver.com>
2023-11-03 22:33:29 +0900

    model schema 세팅 진행 - user, cart, product, Order 4목록 schema 세팅

---
+++ Model/Product.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = Schema({
+  sku: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  category: { type: Array, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Object, required: true },
+  status: { type: String, default: "active" },
+  isDeleted: { type: Boolean, default: false } // 삭제상품 가리기용
+}, { timestamps: true });
+
+productSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.__v;
+  delete obj.updateAt;
+  delete obj.createAt;
+  return obj;
+}
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
