46e93bf51e4c5a250ac322ea03313f7deaaf8eec
jas8157 <jas8157@naver.com>
2023-11-07 00:40:07 +0900

    회원가입 기능 추가 - 포스트맨으로 어드민 계정 생성 - router / user api 작업

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const indexRouter = require('./Routes/index')
 
 require('dotenv').config();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use('/api', indexRouter)
 
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
 mongoose
