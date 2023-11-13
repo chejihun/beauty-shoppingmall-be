67642373f0b3fcfe0c3c17ed8fd0161203261f36
jas8157 <jas8157@naver.com>
2023-11-13 18:41:00 +0900

    제품 업데이트 데이터 추가 - 라우터 put 추가 - 제품 수정시 id정보로 제품 찾아 표시

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -26,9 +26,9 @@ productController.createProduct = async (req, res) => {
 productController.getProduct = async (req, res) => {
   try {
     const { page, name } = req.query
-    const SearchConditions = name?{name:{$regex:name, $options:'i'}} : {}
+    const SearchConditions = name ? { name: { $regex: name, $options: 'i' } } : {}
     let query = Product.find(SearchConditions)
-    
+
     const productList = await query.exec()
     res.status(200).json({ status: 'success', data: productList });
   } catch (error) {
@@ -36,4 +36,21 @@ productController.getProduct = async (req, res) => {
   }
 }
 
+productController.updateProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const { sku, name, size, image, category, description, stock, status, price } = req.body
+    const product = await Product.findByIdAndUpdate(
+      { _id: productId },
+      { sku, name, size, image, category, description, stock, status, price },
+      { new: true }
+    );
+    if (!product) {
+      throw new Error("상품이 존재하지 않습니다")
+    }
+    res.status(200).json({ status: 'success', data: product })
+  } catch (error) {
+    res.status(400).json({ status: 'fail', error: error.message });
+  }
+}
 module.exports = productController;
